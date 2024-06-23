5fea03bc1dc83ec7024063a79f8ca58852b0b6df
kirilovStefan <stefan.kirilov2@abv.bg>
2024-06-23 20:25:03 +0300

    Add digital calculator html,css and JS

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 function appendToScreen(value) {
     document.getElementById('screen').value += value;
-}
-
-function clearScreen() {
+  }
+  
+  function clearScreen() {
     document.getElementById('screen').value = '';
-}
-
-function calculate() {
-    let screen = document.getElementById('screen');
-    try {
-        screen.value = eval(screen.value);
-    } catch (error) {
-        screen.value = 'Error';
-    }
-}
+  }
+  
+  function calculate() {
+    let result = eval(document.getElementById('screen').value);
+    document.getElementById('screen').value = result;
+  }
+  
